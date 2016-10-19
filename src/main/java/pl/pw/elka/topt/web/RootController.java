0606a32e5a8640d5004717a32a9aefa172f4a5f1
pablo09 <pzeszko@gmail.com>
2016-10-19 10:49:50 +0200

    Project structure change

--- src/main/java/pl/pw/elka/topt/web/RootController.java
+++ src/main/java/pl/pw/elka/topt/web/RootController.java
@@ -2,11 +2,13 @@ package pl.pw.elka.topt.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Pawel on 2016-10-12.
  */
 @RestController
+@RequestMapping("api/")
 public class RootController {
 
     @GetMapping
